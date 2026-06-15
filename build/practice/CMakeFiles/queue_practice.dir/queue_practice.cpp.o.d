e4494e05307e9e91bd06fdc3efa7770dad1012ed
Bikash Malla <bikashmalla80@gmail.com>
2026-06-14 20:42:41 -0500

    message

---
+++ build/practice/CMakeFiles/queue_practice.dir/queue_practice.cpp.o.d
@@ -0,0 +1,2 @@
+practice/CMakeFiles/queue_practice.dir/queue_practice.cpp.o: \
+  /Users/bikash_malla/Desktop/SICK-defect-vision/practice/queue_practice.cpp
---
+++ build/practice/CMakeFiles/threads_practice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threads_practice.dir/threads_practice.cpp.o"
+  "CMakeFiles/threads_practice.dir/threads_practice.cpp.o.d"
+  "threads_practice"
+  "threads_practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/threads_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
